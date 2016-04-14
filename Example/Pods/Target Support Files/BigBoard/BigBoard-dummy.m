5490c3bfde371e4e906ad31f05a9b8d3c070a645
Dalton <daltonhint4@gmail.com>
2016-04-14 01:28:39 -0500

    #master base cocoapods skeleton setup

---
+++ Example/Pods/Target Support Files/BigBoard/BigBoard-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BigBoard : NSObject
+@end
+@implementation PodsDummy_BigBoard
+@end
---
+++ Example/Pods/Target Support Files/BigBoard/BigBoard-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double BigBoardVersionNumber;
+FOUNDATION_EXPORT const unsigned char BigBoardVersionString[];
+
